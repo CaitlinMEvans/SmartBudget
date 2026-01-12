6ae4f594d88786af3960e4d521a064fbcea4e303
Caitlin Evans <caitmsevans@gmail.com>
2026-01-12 15:19:26 -0700

    Init REACT Repo with boiler plate update

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ return (
+    <div>
+      <h1>SmartBudget</h1>
+      <p>Student Expense Tracking and Financial Planner</p>
+    </div>
+  );
+}
+
+export default App;

8c860d0c34a4fc025aec656bbad3b34242631aa0
Caitlin Evans <caitmsevans@gmail.com>
2026-02-05 12:58:12 -0700

    updated me route and me auth

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,19 +1,11 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, me, updatePassword } from "../controllers/auth.controller.js";
 import { requireAuth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
-// TEST protected route
-router.get("/me", requireAuth, (req, res) => {
-  res.json({
-    message: "Authenticated",
-    userId: req.user.userId,
-  });
-});
 
 // Account/Profile endpoint
 router.get("/me", requireAuth, me);

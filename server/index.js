38b188d60a9e9495461e65a9903cb2a5d6b25dce
Caitlin Evans <caitmsevans@gmail.com>
2026-02-10 10:37:31 -0700

    deploy update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://smart-budget-one.vercel.app/', 
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // ==========================
